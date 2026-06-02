e030200d293637ed56d0b22cfed42c2d6aee6781
DaniDuese <dan@x70.one>
2026-06-03 00:23:22 +0200

    chore: move favicon and icon assets to public

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -48,7 +48,7 @@ export function SEO({ seo }: { seo?: SEOProps }) {
       <Head>
         <meta name="googlebot" content="index,follow" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         {seo.path ? (
           <link
